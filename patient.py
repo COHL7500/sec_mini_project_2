bdcfb61d60a98702fd0b6c33f617cb569572a35f
cohl7500 <cohl7500@gmail.com>
2023-11-05 18:10:49 +0100

    refactor: minor tweaks for cleaner code

--- patient.py
+++ patient.py
@@ -6,13 +6,13 @@ from config import Config
 
 
 class Patient:
-    def __init__(self, config, port, id):
-        self.conn = []
+    def __init__(self, config: Config, port: int, id: int):
         self.id = id
         self.port = port
         self.config = config
-        self.msgs = []
-        self.conn_amount = 0
+        self.messages = []
+        self.total_connected = 0
+        self.msg_tag = f"({self.id} : {self.port})"
 
         threading.Thread(target=self.setup_connection).start()
 
@@ -22,45 +22,46 @@ class Patient:
 
         while True:
             try:
-                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                conn = ctx.wrap_socket(sock, server_hostname=self.config.hospital_ip)
+                socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                secured_conn = ctx.wrap_socket(socket_obj, server_hostname=self.config.hospital_ip)
 
-                conn.bind((self.config.hospital_ip, self.config.num_port + self.id))
-                conn.connect((self.config.hospital_ip, self.port))
+                secured_conn.bind((self.config.hospital_ip, self.config.num_port + self.id))
+                secured_conn.connect((self.config.hospital_ip, self.port))
 
-                self.conn = conn
-                self.conn_amount += 1
+                self.total_connected += 1
+                self.secured_conn = secured_conn
 
-                print(f"connected to peer {self.id} ({self.port})")
+                print(f"Successfully connected to {self.msg_tag}")
+
                 return
 
             except socket.error:
-                print(f"could not connect to peer {self.id} ({self.port}), retrying in 5s...")
-                sleep(5)
+                print(f"ERROR: Could not connect to {self.msg_tag}, will try again in 3 seconds")
+                sleep(3)
 
-    def send_msg(self, msg):
-        self.conn.sendall(msg.encode("utf-8"))
-
-    def get_msg(self):
-        while not self.msgs:
+    def get_message(self):
+        while not self.messages:
             sleep(1)
-        return self.msgs.pop()
+        return self.messages.pop()
 
-    def rcv_msg(self, conn, addr):
+    def send_message(self, msg):
+        self.secured_conn.sendall(msg.encode("utf-8"))
+
+    def receive_message(self, conn: ssl.SSLSocket, addr):
         context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
         context.load_cert_chain(Config.cert_path, Config.p_key_path)
         sock = context.wrap_socket(conn, server_side=True)
-        self.conn_amount += 1
+        self.total_connected += 1
 
         while True:
             msg = sock.recv(1024)
 
             if len(msg) == 0:
-                print(f"server at peer {self.id} : {self.port} stopped! Terminating process.")
+                print(f"ERROR: Lost connection to {self.msg_tag}! Terminating process.")
                 break
 
-            self.msgs.append(msg)
+            self.messages.append(msg)
 
         sock.close()
-        self.conn.close()
-        self.conn_amount = 0
+        self.secured_conn.close()
+        self.total_connected = 0
--- server.py
+++ server.py
@@ -1,17 +1,16 @@
 import socket
 from threading import Thread
 from time import sleep
-
 import PeerFactory
 from config import Config
-from patient import Patient
 
 
 class Server(Thread):
-    def __init__(self, config, peer_factory: PeerFactory.PeerFactory):
+    def __init__(self, config: Config, peer_factory: PeerFactory.PeerFactory):
         super(Server, self).__init__()
+        self.server_socket = None
         self.config = config
-        self.peers = []
+        self.peer_list = []
         self.peer_factory = peer_factory
 
         self.setup_server_socket()
@@ -23,12 +22,12 @@ class Server(Thread):
             self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.server_socket.bind((self.config.hospital_ip, self.config.num_port))
             self.server_socket.listen(self.config.peer_amount)
-            print("server running...")
 
         except socket.error:
             print("failed socket error")
             exit()
 
+        print("server running...")
         self.setup_peers()
 
     def run(self):
@@ -36,37 +35,38 @@ class Server(Thread):
             client_socket, address = self.server_socket.accept()
             peer = self.find_peer(int(address[1]))
 
-            thread = Thread(target=peer.rcv_msg, args=(client_socket, address))
+            thread = Thread(target=peer.receive_message, args=(client_socket, address))
             thread.start()
 
             print(f"peer {address[1]} connected to local server")
 
     def setup_peers(self):
+
         for client_port in (range(
                 self.config.hospital_port,
                 self.config.hospital_port + self.config.max_port * self.config.peer_amount + 1,
                 self.config.max_port)
         ):
+            peer_id = len(self.peer_list) + 1
 
             if client_port != Config.num_port:
-                self.peers.append(self.peer_factory.create_peer(self.config, client_port, len(self.peers) + 1))
-               # self.peers.append(Patient(self.config, client_port, len(self.peers) + 1))
+                self.peer_list.append(self.peer_factory.create_peer(self.config, client_port, peer_id))
 
     def wait_for_peers(self):
         while True:
-            for p in self.peers:
-                if p.conn_amount != 2:
+            for p in self.peer_list:
+                if p.total_connected != 2:
                     sleep(2)
                     break
             else:
-                print("peers are now all connected...")
+                print("Successfully connected all peers!")
                 return
 
+    def get_message_from_peer(self, peer: int):
+        return self.peer_list[peer].get_message()
+
     def find_peer(self, port: int):
-        return next(i for i in self.peers if ((port % i.port) <= Config.peer_amount))
+        return next(i for i in self.peer_list if ((port % i.port) <= Config.peer_amount))
 
-    def send_msg_peer(self, peer: int, msg):
-        self.peers[peer].send_msg(msg)
-
-    def get_msg_peer(self, peer: int):
-        return self.peers[peer].get_msg()
+    def send_message_to_peer(self, peer: int, msg):
+        self.peer_list[peer].send_message(msg)
